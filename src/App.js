3b62c366c96a177dcb89287403b471247fc34384
Andrew <Andrew@Andrews-MBP.home>
2021-01-02 12:31:31 -0500

    fixed api fetch bug

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React, { Component } from "react";
-import config from './config'
+import config from "./config";
 import Context from "./Context";
-import NotePageNav from './components/NotePageNav/NotePageNav'
-import NoteListNav from './components/NoteListNav/NoteListNav'
-import NoteListMain from './components/NoteListMain/NoteListMain'
-import NotePageMain from './components/NotePageMain/NotePageMain'
+import NotePageNav from "./components/NotePageNav/NotePageNav";
+import NoteListNav from "./components/NoteListNav/NoteListNav";
+import NoteListMain from "./components/NoteListMain/NoteListMain";
+import NotePageMain from "./components/NotePageMain/NotePageMain";
 import "./App.css";
 import { Route, Link } from "react-router-dom";
 
@@ -14,31 +14,30 @@ class App extends Component {
     notes: [],
     folders: [],
   };
-// fetch note api when page renders
+  // fetch note api when page renders
   componentDidMount() {
     Promise.all([
       fetch(`${config.API_ENDPOINT}/notes`),
-      fetch(`${config.API_ENDPOINT}/folders`)
+      fetch(`${config.API_ENDPOINT}/folders`),
     ])
-    .then (([notesRes, foldersRes])=> {
-      if (!notesRes.ok)
-      return notesRes.json().then(e=>Promise.reject(e));
-      if (!foldersRes.ok)
-      return foldersRes.json().then(e=>Promise.reject(e));
+      .then(([notesRes, foldersRes]) => {
+        if (!notesRes.ok) return notesRes.json().then((e) => Promise.reject(e));
+        if (!foldersRes.ok)
+          return foldersRes.json().then((e) => Promise.reject(e));
 
-      return Promise.all([notesRes.json(), foldersRes.json()]);
-    })
-    .then(([notes, folders])=> {
-      this.setState({notes, folders})
-    })
-    .catch(error=>console.error({error}))
+        return Promise.all([notesRes.json(), foldersRes.json()]);
+      })
+      .then(([notes, folders]) => {
+        this.setState({ notes, folders });
+      })
+      .catch((error) => console.error({ error }));
   }
   // delete note
-  handleDeleteNote= noteId => {
+  handleDeleteNote = (noteId) => {
     this.setState({
-      note: this.state.notes.filter(note=> note.id !== noteId)
-    })
-  }
+      note: this.state.notes.filter((note) => note.id !== noteId),
+    });
+  };
 
   // render nav routes
   renderNavRoutes() {
@@ -57,31 +56,26 @@ class App extends Component {
   renderMainRoutes() {
     return (
       <>
-      {['/', '/folder/:folderId'].map(path => (
-        <Route 
-        exact
-        key={path}
-        path={path}
-        component={NoteListMain}
-        />
-      ))}
-      <Route path='.note/:noteId' component={NotePageMain} />
+        {["/", "/folder/:folderId"].map((path) => (
+          <Route exact key={path} path={path} component={NoteListMain} />
+        ))}
+        <Route path="/note/:noteId" component={NotePageMain} />
       </>
-    )
+    );
   }
 
   render() {
     return (
       <Context.Provider value={this.state}>
         <div className="App">
-          <nav className='App_nav'>{this.renderNavRoutes()}</nav>
-          <header className='App_header'>
+          <nav className="App_nav">{this.renderNavRoutes()}</nav>
+          <header className="App_header">
             <h1>
-              <Link to='/'>Noteful</Link>
+              <Link to="/">Noteful</Link>
             </h1>
           </header>
-          <main className='App_main'>{this.renderMainRoutes()}</main>
-          </div>
+          <main className="App_main">{this.renderMainRoutes()}</main>
+        </div>
       </Context.Provider>
     );
   }
--- src/components/Note/Note.js
+++ src/components/Note/Note.js
@@ -1,53 +1,59 @@
-import React from 'react'
-import {Link} from 'react-router-dom'
-import Context from '../../Context'
-import config from '../../config'
-import {format} from 'date-fns'
+import React from "react";
+import { Link } from "react-router-dom";
+import Context from "../../Context";
+import config from "../../config";
 
-class Note extends React.Component{
-    static defaultProps = {
-        onDeleteNote: ()=> {}
-    }
-    static contextType = Context
+class Note extends React.Component {
+  static defaultProps = {
+    onDeleteNote: () => {},
+  };
+  static contextType = Context;
 
-    handleClickDelete = e => {
-        e.preventDefault();
-        const noteId = this.props.noteId
+  handleClickDelete = (e) => {
+    e.preventDefault();
+    const noteId = this.props.noteId;
 
-        fetch(`${config.API_ENDPOINT}/notes/${noteId}`, {
-            method: 'DELETE',
-            headers: {
-                'content-type': 'application/json'
-            }
-        })
-        .then(res=> {
-            if (!res.ok)
-            return res.json().then(e=>Promise.reject(e))
-            return res.json()
-        })
-        .then(()=>{
-            this.context.deleteNote(noteId)
-            this.props.onDeleteNote(noteId)
-        })
-        .catch(error => {
-            console.error({error})
-        })
-    }
-    render() {
-        const {name, id, modified} = this.props
-        return (
-            <div className='Note'>
-                <h2 className='Note_title'>
-                    <Link to={`/note/${id}`}>{name}</Link>
-                </h2>
-                <button className='Note_delete' type='button' onClick={this.handleClickDelete}>Remove</button>
-                <div className='Note_dates'>
-                    <div className='Note_dates-modified'>Modified{''}<span className='Date'>{format(modified, 'Do MMM YYYY')}</span></div>
-                </div>
-            </div>
-
-        )
-    }
+    fetch(`${config.API_ENDPOINT}/notes/${noteId}`, {
+      method: "DELETE",
+      headers: {
+        "content-type": "application/json",
+      },
+    })
+      .then((res) => {
+        if (!res.ok) return res.json().then((e) => Promise.reject(e));
+        return res.json();
+      })
+      .then(() => {
+        this.context.deleteNote(noteId);
+        this.props.onDeleteNote(noteId);
+      })
+      .catch((error) => {
+        console.error({ error });
+      });
+  };
+  render() {
+    const { name, id, modified } = this.props;
+    return (
+      <div className="Note">
+        <h2 className="Note_title">
+          <Link to={`/note/${id}`}>{name}</Link>
+        </h2>
+        <button
+          className="Note_delete"
+          type="button"
+          onClick={this.handleClickDelete}
+        >
+          Remove
+        </button>
+        <div className="Note_dates">
+          <div className="Note_dates-modified">
+            Modified :{" "}
+            <span className="Date">{new Date(modified).toLocaleString()}</span>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default Note;
